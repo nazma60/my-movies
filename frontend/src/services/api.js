0326691507207b0f05f321a5157e1a91123d2063
nazma60 <70542885+nazma60@users.noreply.github.com>
2025-12-02 21:32:34 -0800

    working files

---
+++ frontend/src/services/api.js
@@ -0,0 +1,19 @@
+const API_KEY = "91885fc93a92781d94e97426f4196b8b";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
